277de24290ce993cdff55d37e4e9f3c988b4571f
mr-exz <root@exzec.ru>
2024-12-27 21:32:26 +0300

    delete redmine 4 support

--- db/migrate/001_create_keys.rb
+++ db/migrate/001_create_keys.rb
@@ -1,21 +1,9 @@
-if Redmine::VERSION.to_s.start_with?('4')
-  class CreateKeys < ActiveRecord::Migration[6.1]
-    def change
-      create_table :keys do |t|
-        t.belongs_to :project, index: true
-        t.string :lock
-        t.string :body
-      end
-    end
-  end
-else
-  class CreateKeys < ActiveRecord::Migration[6.1]
-    def change
-      create_table :keys do |t|
-        t.belongs_to :project, index: true
-        t.string :lock
-        t.string :body
-      end
+class CreateKeys < ActiveRecord::Migration[6.1]
+  def change
+    create_table :keys do |t|
+      t.belongs_to :project, index: true
+      t.string :lock
+      t.string :body
     end
   end
 end
--- db/migrate/003_add_type_to_keys.rb
+++ db/migrate/003_add_type_to_keys.rb
@@ -1,13 +1,5 @@
-if Redmine::VERSION.to_s.start_with?('4')
-  class AddTypeToKeys < ActiveRecord::Migration[4.2]
-    def change
-      add_column :keys, :type, :string, default: 'Password'
-    end
-  end
-else
-  class AddTypeToKeys < ActiveRecord::Migration[6.1]
-    def change
-      add_column :keys, :type, :string, default: 'Password'
-    end
+class AddTypeToKeys < ActiveRecord::Migration[6.1]
+  def change
+    add_column :keys, :type, :string, default: 'Password'
   end
 end
